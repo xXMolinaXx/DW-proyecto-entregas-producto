97615bcc37780d14ffc4f9fe8bb319e188e3d6f6
Kenny Molina <kjmolina@live.com>
2021-06-09 20:24:28 -0600

    Angular
    se agrego angular al proyecto

---
+++ frontendPLATAM/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    C1Component,
+    BienvenidoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
